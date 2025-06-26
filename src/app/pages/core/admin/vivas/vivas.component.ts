391667203439c38b9b1ce3adcde6a6f40676160b
Lorecami <clg.gualinga@yavirac.edu.ec>
2025-06-26 15:21:37 -0500

    vivas

--- src/app/pages/core/admin/vivas/vivas.component.ts
+++ src/app/pages/core/admin/vivas/vivas.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+// Copia y pega esto en vivas.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-vivas',
-  imports: [],
   templateUrl: './vivas.component.html',
-  styleUrl: './vivas.component.scss'
+  styleUrls: ['./vivas.component.scss']
 })
-export class VivasComponent {
+export class VivasComponent implements OnInit {
 
+  formulario!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formulario = this.fb.group({
+      avatar: [''],
+      cellPhone: [''],
+      email: [''],
+      birthdate: [''],
+      identification: [''],
+      lastname: [''],
+      password: [''],
+      personalEmail: [''],
+      phone: [''],
+      name: [''],
+      username: ['']
+    });
+  }
+
+  enviar() {
+    console.log(this.formulario.value);
+  }
 }
